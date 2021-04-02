898edadfa0dcb1af3a68f02ed81acd0e20eab8a8
Krisztian <fodorkrisztian6@gmail.com>
2021-04-02 11:01:32 +0200

    v1v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostItemComponent } from './post-item/post-item.component';
+import { TrucatePipe } from './trucate.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostsComponent,
+    PostItemComponent,
+    TrucatePipe
   ],
   imports: [
     BrowserModule
