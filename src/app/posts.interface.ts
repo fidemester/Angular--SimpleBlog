4c6cadd7a7bbd439fd66f09f59012bab7e5343eb
Zoltan Rakottyai <zoltanrakottyay@gmail.com>
2021-04-14 21:46:31 +0200

    add todo

--- src/app/posts.interface.ts
+++ src/app/posts.interface.ts
@@ -1,6 +1,8 @@
-export interface PostsInterface {
+export interface PostsInterface { 
   userId: number;
   id: number;
   title: string;
   body: string;
 }
+
+// @todo: a fajl kerüljön a posts module mappájába
