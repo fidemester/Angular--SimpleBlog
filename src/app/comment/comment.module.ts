d134699ea3a188b04ce8971a02e2ebf2eb7a4b28
Krisztian <fodorkrisztian6@gmail.com>
2021-04-21 10:25:48 +0200

    -m "v7.2"

--- src/app/comment/comment.module.ts
+++ src/app/comment/comment.module.ts
@@ -4,17 +4,22 @@ import {CommentsService} from "./comments.service";
 import { CommentContainerComponent } from './comment-component/comment-container.component';
 import {CommentItemComponent} from "./comment-item-component/comment-item.component";
 import {MatCardModule} from '@angular/material/card';
-
+import { CommentFormComponent } from './comment-form/comment-form.component';
+import { CommentFormContainerComponent } from './comment-form-container/comment-form-container.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [
   CommentContainerComponent,
-    CommentItemComponent
+    CommentItemComponent,
+    CommentFormComponent,
+    CommentFormContainerComponent
   ],
   imports: [
     CommonModule,
-    MatCardModule
+    MatCardModule,
+    ReactiveFormsModule
   ],
   exports:[
     CommentContainerComponent
--- src/app/comment/comments.service.ts
+++ src/app/comment/comments.service.ts
@@ -16,4 +16,8 @@ export class CommentsService {
     return this.http.get<IComment[]>(CommentsService.API_URL + '/' +'?postId='+ postId)
 
   }
+
+   addComment(comment:IComment):Observable<IComment>{
+    return this.http.post<(IComment)>(CommentsService.API_URL, comment)
+  }
 }
